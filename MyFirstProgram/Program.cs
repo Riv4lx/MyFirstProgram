6986d3a693aa6f98c50ddf2bc0eb4689c855db7b
aaron <aaron@RIVAL>
2024-03-09 19:21:28 +0100

    Ajoutez des fichiers projet.

---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool restart = true;
+
+            while (restart)
+            {
+                Console.Write("Would you like to open the calculator? ");
+                string answer = Console.ReadLine();
+
+
+                if (answer.ToLower() == "yes")
+                {
+                    Console.WriteLine($"Welcome to the Calculator!\nPlease type the first number: ");
+
+                    double num1 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine(num1);
+                    Console.WriteLine("Please, type the second number: ");
+                    double num2 = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("What operation do you want to perform?\n+: Addition.\n-: Substraction\n*: Multiplication.\n/: Division");
+                    string operateur = Console.ReadLine();
+                    double result;
+                    if (operateur == "+")
+                    {
+                        result = num1 + num2;
+                        Console.WriteLine($"The result is {result}");
+                    }
+                    else if (operateur == "-")
+                    {
+                        result = num1 - num2;
+                        Console.WriteLine($"The result is {result}");
+                    }
+                    else if (operateur == "/")
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"The result is {result}");
+                    }
+                    else if (operateur == "*")
+                    {
+                        result = num1 * num2;
+                        Console.WriteLine($"The result is {result}");
+                    }
+                    
+                    else
+                    {
+                        Console.WriteLine("This isn't an operator!");
+                    }
+
+                    
+                }
+
+                else if (answer.ToLower() == "no")
+                {
+                    Console.WriteLine("Have a good day!");
+                }
+                
+
+            }
+
+            
+            Console.ReadKey();
+        }
+            
+        }
+
+    
+}
+ 
+
+
